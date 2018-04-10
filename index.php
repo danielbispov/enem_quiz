affa39d37850811b74f15f241dcf329e18b89f0d
Daniel Bispo <danielbispov@gmail.com>
2018-04-10 13:06:18 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>
+    		Quiz ENEM 2018
+        </title>
+    	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/jquery-3.3.1.js"></script>
+        <script type="text/javascript" src="js/jquery.validate.js"></script>
+    </head>
+
+    <body>
+        <form>
+            <ol>
+                <li>
+                    <div class="form-group">
+                        <label>OUHjsdhfbv</label>
+                        <div>
+                            <input type="radio" name="question_1_answers" id="q1_a" value="a"/>
+                            <label for="q1_a">A) </label>
+                        </div>
+
+                        <div>
+                            <input type="radio" name="question_1_answers" id="q1_b" value="b"/>
+                            <label for="q1_b">B) </label>
+                        </div>
+
+                        <div>
+                            <input type="radio" name="question_1_answers" id="q1_c" value="c"/>
+                            <label for="q1_c">C) </label>
+                        </div>
+
+                        <div>
+                            <input type="radio" name="question_1_answers" id="q1_d" value="d"/>
+                            <label for="q1_d">D) </label>
+                        </div>
+
+                        <div>
+                            <input type="radio" name="question_1_answers" id="q1_e" value="e"/>
+                            <label for="q1_e">E) </label>
+                        </div>
+                    </div>
+                </li>
+            </ol>
+        </form>
+    </body>
+</html>
