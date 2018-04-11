559bb5371fc9e58a07c2ebb8ab39117848f137fe
Daniel Bispo <danielbispov@gmail.com>
2018-04-11 10:31:43 -0400

    Add questions in JSON format

--- index.php
+++ index.php
@@ -14,40 +14,37 @@
 </head>
 
 <body>
+    <div class="container">
+        <header class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="navbar-brand w-100 order-1 order-md-0 dual-collapse2">
+                <h1 class="navbar-brand">
+                    <a class="nav-link" href="#">ENEM Quiz 2018</a>
+                </h1>
+            </div>
+            <ul class="justify-content-end">
+                <li class="nav-item">
+                    <a id="author" class="nav-link" href="#">Autor</a>
+                </li>
+            </ul>
+        </header>
 
-    <header class="navbar navbar-expand-lg navbar-light bg-light">
-
-        <div class="navbar-brand w-100 order-1 order-md-0 dual-collapse2">
-            <h1 class="navbar-brand">
-                <a class="nav-link" href="#">ENEM Quiz 2018</a>
-            </h1>
+        <div class="jumbotron" id="jumbotron">
+            <h1>Preparado para o ENEM 2018?</h1>
+            <p>Aproveite este quiz para exercitar o que você já aprendeu até agora, boa sorte!</p>
         </div>
 
-        <nav class="collapse navbar-collapse mr-sm-2" >
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a id="author" class="nav-link" href="#">Autor</a>
-              </li>
-            </ul>
-        </nav>
-
-    </header>
-
-    <div class="jumbotron" id="jumbotron">
-        <h1>Preparado para o ENEM 2018?</h1>
-        <p>Aproveite este quiz para exercitar o que você já aprendeu até agora, boa sorte!</p>
-    </div>
-
-    <div class="container" id="wrap_content">
-        <form id="quiz" action="correction.php" method="post">
-            <ol>
-                <?php
-                include "questions_mng.php";
-                get_questions();
-                ?>
-            </ol>
-            <input type="submit" value="Enviar Quiz"/>
-        </form>
+        <div class="container" id="wrap_content">
+            <p>Cada questão vale 1 ponto, ao final do quiz você terá a sua pontuação.</p>
+            <form id="quiz" action="correction.php" method="post">
+                <ol>
+                    <?php
+                    include "questions_mng.php";
+                    get_questions();
+                    ?>
+                </ol>
+                <input type="submit" value="Enviar Quiz"/>
+            </form>
+        </div>
     </div>
 </body>
 </html>
